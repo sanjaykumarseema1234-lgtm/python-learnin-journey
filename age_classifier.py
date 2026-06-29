b97ea04d5205030a61052688b3ac7f8e5bffd07c
sanjay kumar <sanjaykumarseema1234@gmail.com>
2026-06-29 08:37:02 +0530

    Create age_classifier.py

---
+++ age_classifier.py
@@ -0,0 +1,9 @@
+name = input("enter the name:")
+age = int(input("enter your age:"))
+
+if age >= 18:
+   print("you are an adult!")
+elif 12<= age <= 17:
+   print("you are a teenager")
+else:
+   print("you are a child")
