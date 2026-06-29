069eb549745be0e48ac2f34cb61fb467801ac449
sanjay kumar <sanjaykumarseema1234@gmail.com>
2026-06-29 08:53:17 +0530

    Add function to determine if number is positive, zero, or negative

---
+++ tell_sign of number.py
@@ -0,0 +1,8 @@
+number = int(input("enter the number:"))
+
+if number >= 1:
+   print("entered number is positive")
+elif number == 0:
+   print("entered number is zero")
+else:
+   print("entered number is negative")
